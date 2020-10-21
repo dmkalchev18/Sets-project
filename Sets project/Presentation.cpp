552413d7e4180d30dafec8f74b7b965aa031f660
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2020-10-21 22:14:43 +0300

    create showMenu()
    still has some work to do

--- Sets project/Presentation.cpp
+++ Sets project/Presentation.cpp
@@ -1,2 +1,17 @@
 #include "Presentation.h"
 #include "Data.h"
+#include <string>
+void showMenu() {
+	for (int i = 0; i < 120; i++) {
+		cout << char(177);
+	}
+	
+	cout << endl;
+	cout << "\t1. Enter a set of numbers" << endl;
+	cout << "\t2. Show the sets" << endl;
+	cout << "\t3. Show the subset"<< endl;
+	cout << "\t4. Show the union" << endl;
+	cout << "\t5. Show the section" << endl;
+	cout << "\t6. Show the difference" << endl;
+	cout << "\t7. Exit" << endl;
+}
--- Sets project/Sets project.cpp
+++ Sets project/Sets project.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
-	InputSetInteger();
+	showMenu();
+	/*InputSetInteger();*/
 }
 
