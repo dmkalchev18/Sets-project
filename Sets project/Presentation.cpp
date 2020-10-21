9422c69e44049fdaf3094a977a2d9fa8fda9e870
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-10-21 17:35:26 +0300

    Create presentation and data layers
    * fix #8

---
+++ Sets project/Presentation.cpp
@@ -0,0 +1,2 @@
+#include "Presentation.h"
+
--- Sets project/Sets project.cpp
+++ Sets project/Sets project.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <math.h>
+#include "Data.h"
+#include "Presentation.h"
 using namespace std;
 
 int main()
 {
-    
+
 }
 
